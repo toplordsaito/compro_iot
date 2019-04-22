55426dd3324b8c75748533a80d53188b98a0aeb3
toplordsaito <42961261+toplordsaito@users.noreply.github.com>
2019-04-22 11:37:02 +0700

    zz

---
+++ iot-firebase/src/components/ControlCard.vue
@@ -0,0 +1,51 @@
+<template>
+  <div @click="handleClick" class="control-card col-6 p-1">
+    <div :class="['row wrapper', {'isOff': !isOn}]">
+      <div class="col-4">
+        <img class="w-100" :src="image" alt="">
+      </div>
+      <div class="col-8 center">
+        <p>
+          {{ name }} is 
+          <b v-if="isOn">on</b>
+          <b v-else>off</b>
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ControlCard',
+  props: ['name', 'isOn', 'image'],
+  methods: {
+    handleClick () {
+      this.$emit('isClick')
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.control-card {
+  .wrapper {
+    padding: 10px 3px;
+    background: rgb(230, 230, 230);
+    margin: 1px;
+  }
+  p {
+    margin-bottom: 0;
+  }
+}
+.center {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.isOff {
+  background: #777777!important;
+  color: #fff;
+}
+</style>
+
---
+++ iot-firebase/src/components/DataCard.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="col-4">
+    <h5>{{ name }}</h5>
+    <h2>{{ value }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DataCard',
+  props: ['name', 'value']
+}
+</script>
