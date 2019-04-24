c6f59e5503c2d2d833ab4871207f50511b1c42c1
Ason <aloha_sonza@hotmail.com>
2019-04-25 00:18:26 +0700

    555

--- calibrate/calibrate.ino
+++ calibrate/calibrate.ino
@@ -1,11 +1,22 @@
 #include <math.h>
 
-void setup() {
-  // put your setup code here, to run once:
+struct Data_for_cal {
+  int count;
+  float sum;
+  float standardDeviation;
+  float data_arr[];
+} data;
 
+void setup() {
+  data.count = 0;
+  data.mean = 0.0;
+  data.standardDeviation = 0.0;
+  int sd = calibrate;
 }
 
+
 void loop() {
+  
   // put your main code here, to run repeatedly:
 
 }
@@ -27,16 +38,45 @@ void calibrate() {
   }
 }
 
-float calculateSD(float data[]) {
-  float sum = 0.0, mean, standardDeviation = 0.0;
-  for(int i=0; i<10; ++i) {
-    sum += data[i];
-  }
+float calculateSDFirstTime(float var) {
+  data.data_arr[data.count] = var;
+  data.count += 1;
+  data.sum += var;
 
-  mean = sum/10;
+  float mean = data.sum/data.count;
 
-  for (i=0; i<10; ++i)
-    standardDeviation += pow(data[i] - mean, 2);
+  for (int i=0; i<data.count; ++i)
+    standardDeviation += pow(data.data_arr[i] - mean, 2);
     
   return sqrt(standardDeviation/10);
+}
+
+char check(float var) {
+  const percen = 2;
+  float 
+}
+
+void extension() {
+  //for tempulature
+  if (tem >= 30) {
+    printf("ขณะนี้อุณหภูมิภายในห้อง %d องศาเซลเซียส %d ฟาเรนไฮ");
+  } else if (tem >= 25) {
+    printf("ขณะนี้อุณหภูมิภายในห้อง %d องศาเซลเซียส %d ฟาเรนไฮ");
+  } else if (tem >= 20) {
+    printf("ขณะนี้อุณหภูมิภายในห้อง %d องศาเซลเซียส %d ฟาเรนไฮ");
+  } else {
+    printf("ขณะนี้อุณหภูมิภายในห้อง %d องศาเซลเซียส %d ฟาเรนไฮ");
+  }
+
+  //for humidity
+  if (humi >= 60) {
+    printf("ขณะนี้ความชื้นสัมพัทธ์ในอากาศิภายในห้อง %d%%");
+    printf("** ความชื้นในอากาศมีค่ามากเกินไปซึ่งป็นความชื้นที่เหมาะสมต่อการเจริญเติบโตของเชื้อรา เป็นความชื้นที่เหมาะสมต่อการเจริญเติบโตของเชื้อรา ซึ่งเชื้อราจะเป็นอันตรายต่อบุคคลที่ป่วยเป็นโรคหอบหืดซึ่งเชื้อราจะเป็นอันตรายต่อบุคคลที่ป่วยเป็นโรคหอบหืดได้ **");
+  } else if (tem >= 25) {
+    printf("ขณะนี้อุณหภูมิภายในห้อง %d องศาเซลเซียส %d ฟาเรนไฮ");
+  } else if (tem >= 20) {
+    printf("ขณะนี้อุณหภูมิภายในห้อง %d องศาเซลเซียส %d ฟาเรนไฮ");
+  } else {
+    printf("ขณะนี้อุณหภูมิภายในห้อง %d องศาเซลเซียส %d ฟาเรนไฮ");
+  }
 }
