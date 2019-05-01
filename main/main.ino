4d4b32cc414792380e9845c505c20dd4f0b51fa9
Kuroishi <40828321+Kuroishi1221@users.noreply.github.com>
2019-05-01 20:30:55 +0700

    update time

--- main/main.ino
+++ main/main.ino
@@ -1,4 +1,4 @@
-//#include <TimeLib.h>
+#include <TimeLib.h>
 #include "DHT.h"
 #include "math.h"
 #include <FirebaseArduino.h>
@@ -6,6 +6,7 @@
 #include <DNSServer.h>
 #include <ESP8266WebServer.h>
 #include <WiFiManager.h>
+#include <WiFiUdp.h>
 
 #define DHTPIN D3 // what digital pin we're connected to
 #define TRIC D0 //input distance sensor
@@ -25,7 +26,23 @@ float humiSD;
 float distSD;
 float temp[10], humi[10], dist[10];
 float tempNow, humiNow, distNow;      //ค่าที่จะอัพขึ้น
-bool LED_status;
+bool LED_status, LED_BEFORE;
+
+// NTP Servers:
+static const char ntpServerName[] = "us.pool.ntp.org";
+const int timeZone = 7;     // Central European Time
+
+WiFiUDP Udp;
+unsigned int localPort = 8888;  // local port to listen for UDP packets
+
+time_t getNtpTime();
+void digitalClockDisplay();
+void printDigits(int digits);
+void sendNTPpacket(IPAddress &address);
+
+
+time_t prevDisplay = 0; // when the digital clock was displayed
+
 void setup() {
   // connect to wifi.
   Serial.begin(9600);
@@ -49,10 +66,14 @@ void setup() {
 
   dht.begin();
   calibrate();
+  
+  LED_status = Firebase.getBool("val/LED");
+  LED_BEFORE = LED_status;
 }
 
 void loop() {
   for (int i=0; i<10; i++) {
+    ledManage();
     tempNow = dht.readTemperature();
     distNow = find_dist();
     humiNow = dht.readHumidity();
@@ -68,7 +89,7 @@ void loop() {
     Serial.println(humiNow);
     Serial.print("Dis : ");
     Serial.println(distNow);
-    delay(10000);
+    delay(1000);
   }
 
   //Temporary SD
@@ -96,11 +117,18 @@ void loop() {
   Firebase.set("SD/tempSD", tempSD);
   Firebase.set("SD/disSD", distSD);
   Firebase.set("SD/humiSD", humiSD);
+//  extension();
+}
+
+void ledManage() {
   LED_status = Firebase.getBool("val/LED");
+
+  if (LED_BEFORE != LED_status) {
   Serial.print("LED status : ");
   Serial.println(LED_status);
   digitalWrite(LED, LED_status);
-//  extension();
+  LED_BEFORE = LED_status;
+  }
 }
 
 void calibrate() {
@@ -118,7 +146,7 @@ void calibrate() {
 
   //Calibrate
   for (int i=0; i<10; i++) {
-    
+    ledManage();
     temp[i] = dht.readTemperature();
     dist[i] = find_dist();
     humi[i] = dht.readHumidity();
@@ -168,6 +196,86 @@ float find_dist() {
 
   return cm;
 }
+
+void digitalClockDisplay()
+{
+  // digital clock display of the time
+  Serial.print(hour());
+  printMinut(minute());
+  printMinut(second());
+  Serial.print(" ");
+  Serial.print(day());
+  Serial.print(".");
+  Serial.print(month());
+  Serial.print(".");
+  Serial.print(year());
+  Serial.println();
+}
+
+void printMinut(int digits) {
+  // utility for digital clock display: prints preceding colon and leading 0
+  Serial.print(":");
+  if (digits < 10)
+    Serial.print('0');
+  Serial.print(digits);
+}
+
+const int NTP_PACKET_SIZE = 48; // NTP time is in the first 48 bytes of message
+byte packetBuffer[NTP_PACKET_SIZE]; //buffer to hold incoming & outgoing packets
+
+time_t getNtpTime()
+{
+  IPAddress ntpServerIP; // NTP server's ip address
+
+  while (Udp.parsePacket() > 0) ; // discard any previously received packets
+  Serial.println("Transmit NTP Request");
+  // get a random server from the pool
+  WiFi.hostByName(ntpServerName, ntpServerIP);
+  Serial.print(ntpServerName);
+  Serial.print(": ");
+  Serial.println(ntpServerIP);
+  sendNTPpacket(ntpServerIP);
+  uint32_t beginWait = millis();
+  while (millis() - beginWait < 1500) {
+    int size = Udp.parsePacket();
+    if (size >= NTP_PACKET_SIZE) {
+      Serial.println("Receive NTP Response");
+      Udp.read(packetBuffer, NTP_PACKET_SIZE);  // read packet into the buffer
+      unsigned long secsSince1900;
+      // convert four bytes starting at location 40 to a long integer
+      secsSince1900 =  (unsigned long)packetBuffer[40] << 24;
+      secsSince1900 |= (unsigned long)packetBuffer[41] << 16;
+      secsSince1900 |= (unsigned long)packetBuffer[42] << 8;
+      secsSince1900 |= (unsigned long)packetBuffer[43];
+      return secsSince1900 - 2208988800UL + timeZone * SECS_PER_HOUR;
+    }
+  }
+  Serial.println("No NTP Response :-(");
+  return 0; // return 0 if unable to get the time
+}
+
+// send an NTP request to the time server at the given address
+void sendNTPpacket(IPAddress &address) {
+  // set all bytes in the buffer to 0
+  memset(packetBuffer, 0, NTP_PACKET_SIZE);
+  // Initialize values needed to form NTP request
+  // (see URL above for details on the packets)
+  packetBuffer[0] = 0b11100011;   // LI, Version, Mode
+  packetBuffer[1] = 0;     // Stratum, or type of clock
+  packetBuffer[2] = 6;     // Polling Interval
+  packetBuffer[3] = 0xEC;  // Peer Clock Precision
+  // 8 bytes of zero for Root Delay & Root Dispersion
+  packetBuffer[12] = 49;
+  packetBuffer[13] = 0x4E;
+  packetBuffer[14] = 49;
+  packetBuffer[15] = 52;
+  // all NTP fields have been given values, now
+  // you can send a packet requesting a timestamp:
+  Udp.beginPacket(address, 123); //NTP requests are to port 123
+  Udp.write(packetBuffer, NTP_PACKET_SIZE);
+  Udp.endPacket();
+}
+
 
 
 //Alert part
